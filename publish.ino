c8bc1887d15663006a4efdd8e05fb4187f3649b3
Rahmiatul12 <102578803+Rahmiatul12@users.noreply.github.com>
2023-05-26 06:09:35 +0800

    Update publish.ino

--- publish.ino
+++ publish.ino
@@ -1,9 +1,101 @@
-void setup() {
-  // put your setup code here, to run once:
+
+#include <ESP32Servo.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+// Update these with values suitable for your network.
+const char* ssid = "Wokwi-GUEST";
+const char* password = "";
+const char* mqtt_server = "broker.hivemq.com";
+const int PinSensor = A0;
+// Sensor Gas
+//#define GAS_PIN 4
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+unsigned long lastMsg = 0;
+#define MSG_BUFFER_SIZE	(50)
+char msg[MSG_BUFFER_SIZE];
+int value = 0;
+
+void setup_wifi() {
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  randomSeed(micros());
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+// menerima data
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Pesan Diterima [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+  if ((char)payload[0] == '1') {
+    digitalWrite(BUILTIN_LED, LOW); 
+    GAS_PIN.write(90);  
+  } else {
+    digitalWrite(BUILTIN_LED, HIGH);  
+  }
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+// fungsi untuk menghubungkan ke broker
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+      client.subscribe("ladeno");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000);
+    }
+  }
+}
 
+void setup() {
+  pinMode(PinSensor, INPUT);
+  pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+}
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+  
+  delay(2000);
+  int levelAir = analogRead(PinSensor);
+  Serial.print("Publish message: ");
+  Serial.println(msg);
+  client.publish("gaskeun", msg); // fungsi untuk publish ke broker
 }
